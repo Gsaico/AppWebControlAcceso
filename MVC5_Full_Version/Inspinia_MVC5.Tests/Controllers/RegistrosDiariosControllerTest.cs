45894d7f8e5aa7c05eaec00e569937bdd1753268
Jorge Klauz Valdivia Davalos <jovad06@gmail.com>
2016-11-15 06:38:11 -0500

    PruebasUnitarias Completa a los registros Diaros del colaborador
    se a creado metodos para probar las posibilidades de los colaboradores
    en la actividad de hacer su registro todos los dias.

--- MVC5_Full_Version/Inspinia_MVC5.Tests/Controllers/RegistrosDiariosControllerTest.cs
+++ MVC5_Full_Version/Inspinia_MVC5.Tests/Controllers/RegistrosDiariosControllerTest.cs
@@ -12,6 +12,7 @@ namespace Inspinia_MVC5.Tests.Controllers
     [TestClass]
     public class RegistrosDiariosControllerTest
     {
+
         [TestMethod]
         public void Test_GetColaboradoresTopCinco()
         {
@@ -24,7 +25,7 @@ namespace Inspinia_MVC5.Tests.Controllers
             // Assert
             Assert.IsNotNull(result);
         }
-
+        /*
         [TestMethod]
         public void Test_GetColaboradorDesconocido()
         {
@@ -49,6 +50,93 @@ namespace Inspinia_MVC5.Tests.Controllers
             // Assert
             Assert.AreEqual("COLABORADOR NO REGISTRADO", result.ViewBag.desconocido);
             
+        }
+        */
+        [TestMethod]
+        public void Test_GetColaboradorById_ColaboradorNoregistrado()
+        {//Colaborador no registrado en el sistema
+            // Arrange
+            RegistrosDiariosController controller = new RegistrosDiariosController();
+
+            // Act
+            PartialViewResult result = controller.GetColaboradorById("00000000") as PartialViewResult;
+
+            // Assert
+            Assert.AreEqual("COLABORADOR NO REGISTRADO", result.ViewBag.desconocido);
+
+        }
+
+        [TestMethod]
+        public void Test_GetColaboradorById_ColaboradorEstadoDesactivo()
+        {
+            //colaborador que esta registrado pero que esta desactivo
+            // Arrange
+            RegistrosDiariosController controller = new RegistrosDiariosController();
+
+            // Act
+            PartialViewResult result = controller.GetColaboradorById("66666666") as PartialViewResult;
+
+            // Assert
+            Assert.AreEqual("ACCESO DENEGADO", result.ViewBag.estado);
+
+        }
+
+        [TestMethod]
+        public void Test_GetColaboradorById_ColaboradorEstadoActivoIngreso_ContratoVigente()
+        {
+            //colaborador que esta registrado y registra su Ingreso con contrato Vigente
+            // Arrange
+            RegistrosDiariosController controller = new RegistrosDiariosController();
+
+            // Act
+            PartialViewResult result = controller.GetColaboradorById("11111111") as PartialViewResult;
+
+            // Assert
+            Assert.AreEqual("ACCESO REGISTRADO", result.ViewBag.estado, "INGRESO",result.ViewBag.ingresosalida);
+
+        }
+        [TestMethod]
+        public void TTest_GetColaboradorById_ColaboradorEstadoActivoSalida_ContratoVigente()
+        {
+            //colaborador que esta registrado y ya  registro su Ingreso  y esta registrando su SALIDA pero con contrato activo
+            // Arrange
+            RegistrosDiariosController controller = new RegistrosDiariosController();
+
+            // Act
+            PartialViewResult result = controller.GetColaboradorById("11111111") as PartialViewResult;
+
+            // Assert
+            Assert.AreEqual("ACCESO REGISTRADO", result.ViewBag.estado, "SALIDA", result.ViewBag.ingresosalida);
+
+        }
+
+        [TestMethod]
+        public void TTest_GetColaboradorById_ColaboradorEstadoActivo_YaMarcoIngresoYSalida_ContratoVigente()
+        {
+            //colaborador que esta registrado y ya  registro su Ingreso  y salida pero con contrato vigente
+            // Arrange
+            RegistrosDiariosController controller = new RegistrosDiariosController();
+
+            // Act
+            PartialViewResult result = controller.GetColaboradorById("11111111") as PartialViewResult;
+
+            // Assert
+            Assert.AreEqual("UD YA NO PUEDE INGRESAR A LAS INSTALACIONES EL DIA DE HOY",result.ViewBag.estado);
+
+        }
+        [TestMethod]
+        public void TTest_GetColaboradorById_ColaboradorEstadoActivo_ContratoVencido()
+        {
+            //colaborador que esta registrado y su contrato esta vencido
+            // Arrange
+            RegistrosDiariosController controller = new RegistrosDiariosController();
+
+            // Act
+            PartialViewResult result = controller.GetColaboradorById("99999999") as PartialViewResult;
+
+            // Assert
+            Assert.AreEqual("ACCESO DENEGADO", result.ViewBag.estado);
+
         }
     }
 }
