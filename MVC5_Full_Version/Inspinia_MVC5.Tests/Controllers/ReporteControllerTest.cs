fb4d13232e87c243f823e3230346cec140682087
Jorge Klauz Valdivia Davalos <jovad06@gmail.com>
2016-11-20 14:09:45 -0500

    Pruebas a los reportes
    NO FUNCIONA LAS PRUEBAS DE ESTA PARTE PORQUE NO PUEDO LEVANTAR EL
    REPORTING SERVER CON LAS PRUEBAS

---
+++ MVC5_Full_Version/Inspinia_MVC5.Tests/Controllers/ReporteControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Inspinia_MVC5.Controllers;
+using System.Web.Mvc;
+
+namespace Inspinia_MVC5.Tests.Controllers
+{
+    /// <summary>
+    /// Descripción resumida de ReporteControllerTest
+    /// </summary>
+    [TestClass]
+    public class ReporteControllerTest
+    {
+        [TestMethod]
+        public void ShowReportRegistro()
+        {
+            // Arrange
+            ReportesController controller = new ReportesController() ;
+
+            // Act
+            ViewResult result = controller.ShowReportRegistroControlDeAsistenciasXDNI() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void ShowReportNumeroColaboradoresEnLasInstalacionesHoy()
+        {//NO FUNCIONAN ESTAS PRUEBAS PORQUE NO ESTA VIGENTE LOS REPORTES
+            // Arrange
+            ReportesController controller = new ReportesController();
+
+            // Act
+            ViewResult result = controller.ShowReportNumeroColaboradoresEnLasInstalacionesHoy() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
