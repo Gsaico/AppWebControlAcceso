7be8c22c67de86495d11200dc15c1883a6400f3a
Percy <gsaico@icloud.com>
2016-11-25 22:11:55 -0500

    OK Proyecto Concluido Para exposicion 25 11 2016

--- MVC5_Full_Version/Inspinia_MVC5.Tests/Controllers/ReporteControllerTest.cs
+++ MVC5_Full_Version/Inspinia_MVC5.Tests/Controllers/ReporteControllerTest.cs
@@ -7,40 +7,46 @@ using System.Web.Mvc;
 
 namespace Inspinia_MVC5.Tests.Controllers
 {
-    /// <summary>
-    /// Descripción resumida de ReporteControllerTest
-    /// </summary>
+
     [TestClass]
+
     public class ReporteControllerTest
-    {   [TestMethod]
-        public void ShowReportRegistro()
+    {
+        [TestMethod]
+        public void SearchtareoColaboradoresXDNI()
         {//REALIZADO POR: JORGE KLAUZ VALDIVIA DAVALOS
          //PRUEBA TRATA: Prueba el método SHOWREPORT REGISTROCONTROL DE ASISTENCIA, se prueba su funcionamiento
 
             // Arrange
-            var controller = new ReportesController() ;
-
+            var controller = new ReportesController();
+            string DNI = "11111111";
+            string Mes = "09";
+            string año = "2016";
             // Act
             //ViewResult result = controller.ShowReportRegistroControlDeAsistenciasXDNI() as ViewResult;
-            var result = controller.ShowReportRegistroControlDeAsistenciasXDNI()  as RedirectResult;
-            
+            var result = controller.SearchTareoColaboradoresXDNI(DNI, Mes, año);
+
             // Assert
-            Assert.IsNotNull(result.Url);
+            Assert.IsNotNull(result);
         }
+
         [TestMethod]
-        public void ShowReportNumeroColaboradoresEnLasInstalacionesHoy()
+        public void SearchRegistroControlDeAsistenciasXDNI()
         {//REALIZADO POR: JORGE KLAUZ VALDIVIA DAVALOS
-         //PRUEBA TRATA: Prueba el método SHOWREPORT NUMERO COLABORADORES, se prueba su funcionamiento
+         //PRUEBA TRATA: Prueba el método SHOWREPORT REGISTROCONTROL DE ASISTENCIA, se prueba su funcionamiento
 
             // Arrange
             var controller = new ReportesController();
-
+            string DNI = "11111111";
+            string Mes = "09";
+            string año = "2016";
             // Act
             //ViewResult result = controller.ShowReportRegistroControlDeAsistenciasXDNI() as ViewResult;
-            var result = controller.ShowReportNumeroColaboradoresEnLasInstalacionesHoy() as RedirectResult;
+            var result = controller.SearchRegistroControlDeAsistenciasXDNI(DNI, Mes, año);
 
             // Assert
-            Assert.IsNotNull(result.Url);
+            Assert.IsNotNull(result);
         }
+
     }
 }
